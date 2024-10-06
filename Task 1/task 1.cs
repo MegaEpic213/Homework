d927ebc6e6000d37f1cf09948c79c73ae7e3923a
Омар Абдул <omarabdul471@gmail.com>
2024-10-07 04:32:00 +0500

    Добавьте файлы проекта.

---
+++ Task 1/task 1.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main()
+    {
+        ConsoleColor originalColor = Console.ForegroundColor;
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Солнечный зайчик взломал потолок");
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("Солнечный зайчик взломал потолок.\n" +
+                          "Закатился камешек на гору.\n" +
+                          "Пряная косточка свежего горя\n" +
+                          "Верно и яростно канула\n" +
+                          "В янтарную лету заслуженного долголетия.\n" +
+                          "Солнечный зайчик взорвал потолок.\n" +
+                          "Кончился почерк. Угасли дожди.\n" +
+                          "Стихло безмолвие.\n" +
+                          "Родина настала.\n");
+
+        Console.ForegroundColor = originalColor;
+    }
+}
